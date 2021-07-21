ebb7404efa646d81a850c71be3e4ac1b977c8f58
Vertipae <33002993+Vertipae@users.noreply.github.com>
2021-07-21 18:56:58 +0300

    Add main layout

---
+++ src/components/FoodComponentlist.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div v-for="obj in componentData" :key="obj.header">
+      <div class="row">
+        <div class="header">{{ obj.header }}</div>
+        <div class="header">Yks.</div>
+      </div>
+
+      <div class="row" v-for="component in obj.components" :key="component">
+        <div>{{ component }}</div>
+        <div>g</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      componentData: [
+        {
+          header: "Perusravintoaineet",
+          components: [
+            "alkoholi",
+            "energia",
+            "hiilihydraatti",
+            "proteiini",
+            "rasva"
+          ]
+        },
+        {
+          header: "Vitamiinit",
+          components: [
+            "E-vitamiini",
+            "D-vitamiini",
+            "K-vitamiini",
+            "Karotenoidit",
+            "A-vitamiini",
+            "Niasiini",
+            "B12-vitamiini"
+          ]
+        }
+      ]
+    }
+  }
+}
+    
+</script>
+
+<style scoped>
+  .row {
+    display: grid;
+    grid-template-columns: 90% 10%;
+  }
+
+  .flexRow {
+    display: flex;
+  }
+
+  .header {
+    font-weight: bold;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <SearchInput />
+
+    <div class="dataContainer">
+      <div class="foodComponentContainer">
+        <FoodComponentlist />
+      </div>
+
+      <div class="foodContainer">
+        <FoodList />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import SearchInput from "@/components/SearchInput.vue";
+import FoodComponentlist from "@/components/FoodComponentlist.vue";
+import FoodList from "@/components/FoodList.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    SearchInput,
+    FoodComponentlist,
+    FoodList
   },
 };
 </script>
+
+<style lang="scss">
+  .dataContainer {
+    display: flex;
+  }
+
+  .foodComponentContainer {
+    background-color: red;
+    flex-grow: 1;
+  }
+
+  .foodContainer {
+    background-color: green;
+    flex-grow: 9;
+  }
+</style>
