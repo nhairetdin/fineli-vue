f9c0e5afcf4b26d08a5acd82edaeb50054d1bede
Nico Hairetdin <hairetdin.nico@gmail.com>
2021-08-01 12:00:49 +0300

    Highlight search string in results

--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="foodRow" v-for="row in foodDataDisplay" :key="row.foodid">{{ row.foodname }}</div>
+    <div class="foodRow" v-for="row in foodDataDisplay" :key="row.foodid" v-html="highlight(row.foodname)"></div>
   </div>
 </template>
 
@@ -14,8 +14,16 @@ export default {
       return this.$store.state.foodData
         .filter(food => food.foodname.includes(keyword))
         .slice(-50)
-    }
+    },
   },
+  methods: {
+    highlight(str) {
+      const keyword = this.$store.state.filterKeyword
+        .toUpperCase()
+
+      return str.replaceAll(keyword, `<span style="color: #eedf90;">${keyword}</span>`)
+    }
+  }
 }
 </script>
 
